00ad2044c139e738e558c8676d4cd883aee49900
abregorivas <abregorivas@gmail.com>
2016-12-28 18:15:52 -0800

    updated navbar

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,19 @@
-
+// window.onload = function() {
+//   var
+//   // canvas = document.getElementById("canvas"),
+//   canvas = document.createElement('canvas'),
+//       context = canvas.getContext("2d");
+//
+// document.body.appendChild(canvas),
+// // document.canvas.style.width = 500;
+//
+//   context.beginPath();
+//   context.moveTo(100, 100);
+//   context.lineTo(300, 100);
+//   context.lineTo(300, 200);
+//   context.lineTo(100, 200);
+//
+//   context.closePath();
+//   context.stroke();
+//
+// };
